8955061b7f6d091ff3c0370a350c49dcd7c020aa
MoremOremoRemorE <1334538961@qq.com>
2019-08-29 22:59:55 +0800

    分离

--- src/main/java/com/jin/jin/controller/LoginController.java
+++ src/main/java/com/jin/jin/controller/LoginController.java
@@ -19,6 +19,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 @Controller
 public class LoginController {
 
